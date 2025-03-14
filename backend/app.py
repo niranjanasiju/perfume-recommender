92e9002aef44b90d60887ad788a8e6605d325a01
niranjana <niranjanasiju92@gmail.com>
2025-03-14 18:10:22 +0530

    modified app.py

--- backend/app.py
+++ backend/app.py
@@ -12,8 +12,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 api_key = os.getenv("GEMINI_API_KEY")
-if not api_key:
-    api_key = "AIzaSyDcrxIbTSnvRuiYhQv-PvrIQtge0HERmDo"
+
 
 genai.configure(api_key=api_key)
 model = genai.GenerativeModel("gemini-2.0-flash")
